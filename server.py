abf186987eb01e471cf21277b90a529387472b16
Студент <student@ad.anosov.ru>
2025-06-11 12:15:39 +0500

    Я починиль :)

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from extensions import db
+import models
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+migrate = Migrate(app, db)
+
+@app.route("/")
+def index():
+    return render_template("index.html", current_page="main")
+
+@app.route("/films")
+def films():
+    return render_template("films.html", current_page="films")
+@app.route("/halls")
+def halls():
+    return render_template("halls.html", current_page="halls")
+def main():
+    app.run("0.0.0.0", port=5000, debug=True)
+    
+
+
+if __name__ == "__main__":
+    main()
