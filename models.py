b1f62d94618951215fffc274f017cedf4ce1f194
Nasir0v-student <nasirovroman604@gmail.com>
2025-06-13 20:37:26 +0500

    Добавил добавление и удаление новых залов

--- models.py
+++ models.py
@@ -1,11 +1,22 @@
 from extensions import db
 
+class Film(db.Model):
+    __tablename__ = 'films'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(100), nullable=False)
+    duration = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Film {self.name}>'
+
 class Halls(db.Model):
     __tablename__ = 'halls'
 
-    number = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, primary_key=True)  # Сделаем number первичным ключом
     capacity = db.Column(db.String(100), nullable=False)
     type = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return f'<Halls {self.title}>'
+        return f'<Hall {self.number}>'
