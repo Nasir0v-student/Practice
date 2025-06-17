953d4b2631835f75a6979bee371303063301a6b8
hollyjammer2 <164227037+hollyjammer2@users.noreply.github.com>
2025-06-17 14:02:51 +0500

    Update models.py

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class Film(db.Model):
 class Halls(db.Model):
     __tablename__ = 'halls'
 
-    number = db.Column(db.Integer, primary_key=True)  # Сделаем number первичным ключом
+    number = db.Column(db.Integer, primary_key=True)
     capacity = db.Column(db.String(100), nullable=False)
     type = db.Column(db.String(100), nullable=False)
 
